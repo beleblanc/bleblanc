8985194c7ea52b1cc5ffe9a612b237f9abf49281
Benoit LeBlanc <ben.leblanc@gmail.com>
2014-02-06 10:42:58 +0200

    Adding footer & social media

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+$("header nav a").click(function() {
+    $('html, body').animate({
+        scrollTop: $($(this).data('scroll-to')).offset().top
+    }, 2000);
+    $('header nav a').removeClass('active');
+    $(this).addClass('active');
+});
